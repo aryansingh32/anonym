a95540fcd08c82e3db17fae822ffdfaa55424136
Aryan Singh <aryan3340s@gmail.com>
2025-11-10 22:54:29 +0530

    Comment out Redis configuration class

--- src/main/java/com/messagingCluster/Messenger/config/RedisConfig.java
+++ src/main/java/com/messagingCluster/Messenger/config/RedisConfig.java
@@ -1,49 +1,49 @@
-package com.messagingCluster.Messenger.config;
+// package com.messagingCluster.Messenger.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
+// import org.springframework.beans.factory.annotation.Value;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.data.redis.connection.RedisConnectionFactory;
+// import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+// import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+// import org.springframework.data.redis.core.RedisTemplate;
+// import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+// import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-@Configuration
-public class RedisConfig {
+// @Configuration
+// public class RedisConfig {
 
-    @Value("${spring.data.redis.host}")
-    private String redisHost;
+//     @Value("${spring.data.redis.host}")
+//     private String redisHost;
 
-    @Value("${spring.data.redis.port}")
-    private int redisPort;
+//     @Value("${spring.data.redis.port}")
+//     private int redisPort;
 
-    @Value("${spring.data.redis.password:}")
-    private String redisPassword;
+//     @Value("${spring.data.redis.password:}")
+//     private String redisPassword;
 
-    @Bean
-    public LettuceConnectionFactory redisConnectionFactory() {
-        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
-        config.setHostName(redisHost);
-        config.setPort(redisPort);
+//     @Bean
+//     public LettuceConnectionFactory redisConnectionFactory() {
+//         RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+//         config.setHostName(redisHost);
+//         config.setPort(redisPort);
         
-        if (redisPassword != null && !redisPassword.isEmpty()) {
-            config.setPassword(redisPassword);
-        }
+//         if (redisPassword != null && !redisPassword.isEmpty()) {
+//             config.setPassword(redisPassword);
+//         }
         
-        return new LettuceConnectionFactory(config);
-    }
+//         return new LettuceConnectionFactory(config);
+//     }
 
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(connectionFactory);
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-        template.setHashKeySerializer(new StringRedisSerializer());
-        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
-        template.afterPropertiesSet();
-        return template;
-    }
-}
+//     @Bean
+//     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+//         RedisTemplate<String, Object> template = new RedisTemplate<>();
+//         template.setConnectionFactory(connectionFactory);
+//         template.setKeySerializer(new StringRedisSerializer());
+//         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+//         template.setHashKeySerializer(new StringRedisSerializer());
+//         template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+//         template.afterPropertiesSet();
+//         return template;
+//     }
+// }
